33e23a214f60c7e6886b8bf7aebf61fa696a3c12
guanbinrui <guanbinrui@dimension.im>
2019-08-09 13:57:34 +0800

    add decrypt script

---
+++ decrypt.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+import os
+from base64 import b64encode, b64decode
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+
+backend = default_backend()
+key = os.urandom(32)
+iv = os.urandom(16)
+
+cipher = Cipher(algorithms.AES(key), modes.GCM(iv), backend=backend)
+encryptor = cipher.encryptor()
+decryptor = cipher.decryptor()
+
+# key and iv
+print(b64encode(key).decode('utf-8'))
+print(b64encode(iv).decode('utf-8'))
+
+with open('./encrypted_actpwds.csv', 'r') as plain:
+    # decrypt
+    joined_ct = ''.join(plain.readlines())
+    ct = b64decode(joined_ct)
+    pt = decryptor.update(b64decode(joined_ct))
+
+    # write
+    decrypted = open('./actpwds.csv', 'w')
+    decrypted.write(pt)
