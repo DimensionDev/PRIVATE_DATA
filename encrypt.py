372085d562b8dd483566781f8d3df0f784c64c45
guanbinrui <guanbinrui@dimension.im>
2019-08-09 13:57:01 +0800

    format

--- encrypt.py
+++ encrypt.py
@@ -6,7 +6,7 @@ from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
 
 backend = default_backend()
-key =  os.urandom(32)
+key = os.urandom(32)
 iv = os.urandom(16)
 
 cipher = Cipher(algorithms.AES(key), modes.GCM(iv), backend=backend)
